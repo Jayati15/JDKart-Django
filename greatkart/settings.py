b10fa280d02b458204715c91abe5dc99cd37e62a
Jayati15 <77896511+Jayati15@users.noreply.github.com>
2022-02-04 19:36:30 +0530

    config

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -166,7 +166,7 @@ MESSAGE_TAGS = {
 
 #SMTP configuration
 EMAIL_HOST= os.getenv('EMAIL_HOST')
-EMAIL_PORT= os.getenv('EMAIL_PORT',cast=int)
+EMAIL_PORT= os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS',cast=bool)
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
