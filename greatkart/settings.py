85fefbaa01a8beac85a89012eb8e046438d5ba96
Jayati15 <77896511+Jayati15@users.noreply.github.com>
2022-02-04 17:57:09 +0530

    config

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -25,7 +25,7 @@ config.encoding = locale.getpreferredencoding(False)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=True,cast=bool)
+DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = []
 
