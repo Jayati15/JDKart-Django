b893c8c3760caad01d1cb5c5c4a3fe97d4d4d735
Jayati15 <77896511+Jayati15@users.noreply.github.com>
2022-02-04 17:23:59 +0530

    config

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import locale
+config.encoding = locale.getpreferredencoding(False)
 from ctypes import cast
 from pathlib import Path
 from decouple import config
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=True,cast=bool)
+DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = []
 
