4e0ade039a97c5f4c3fb337577653cfca01c74b0
Jayati15 <77896511+Jayati15@users.noreply.github.com>
2022-02-04 18:21:19 +0530

    config

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -10,20 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import locale
-
+import environ
 from ctypes import cast
 from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+
+# you have to create .env file in same folder where you are using environ.Env()
+# reading .env file which located in api folder
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-config.encoding = locale.getpreferredencoding(False)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
