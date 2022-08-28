a0a6b453bc6c2b4cba74c49e7e04ec86041e8152
Jayati15 <77896511+Jayati15@users.noreply.github.com>
2022-08-28 17:37:37 +0530

    changes

--- jdkart/settings.py
+++ jdkart/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='django-insecure-_f--=!$08bx=ad3+m1av4)qq+_%l4*b7&1-lko3(!2laysv0j('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
